8682d50d362cf19fdc63b0feacf9510006985b6e
杨山 <shan.yang@cyber-insight.com>
2020-07-15 16:43:06 +0800

    fragment supporting switch test

--- src/components/FragmentSupportingSwitch/FragmentSupportingSwitch.test.js
+++ src/components/FragmentSupportingSwitch/FragmentSupportingSwitch.test.js
@@ -1,36 +1,51 @@
-import React, { Fragment } from "react";
-import { render, screen } from "@testing-library/react";
+import React from "react";
+import { render } from "@testing-library/react";
 import FragmentSupportingSwitch from "./FragmentSupportingSwitch";
-import { Router } from "react-router-dom";
+import { Router, Link, Route } from "react-router-dom";
 import { createMemoryHistory } from "history";
+import userEvent from "@testing-library/user-event";
 
-describe("产品组件测试", () => {
-  test("正确渲染产品名称", () => {
-    const TestComponent = () => {
-      return (
-        <div>
-          <Fragment>
-            <span>this is test text1</span>
-            <Fragment>
-              <p>this is test text2</p>
-              <p></p>
-            </Fragment>
-            <span></span>
-          </Fragment>
-        </div>
-      );
-    };
+const About = () => <h1>You are on the about page</h1>;
+const Home = () => <h1>You are home</h1>;
+const NoMatch = () => <h1>404 Not Found</h1>;
 
+function TestComponent() {
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <FragmentSupportingSwitch>
+        <Route exact path="/" component={Home} />
+        <>
+          <Route path="/about" component={About} />
+          <Route component={NoMatch} />
+        </>
+      </FragmentSupportingSwitch>
+    </div>
+  );
+}
+
+describe("fragment supporting switch testing", () => {
+  test("full app rendering/navigating home page", () => {
     const history = createMemoryHistory();
-    render(
+    const { container, getByText } = render(
       <Router history={history}>
-        <FragmentSupportingSwitch>
-          <TestComponent />
-        </FragmentSupportingSwitch>
+        <TestComponent />
       </Router>
     );
+    expect(container.innerHTML).toMatch("You are home");
+    userEvent.click(getByText(/about/i));
+    expect(container.innerHTML).toMatch("You are on the about page");
+  });
 
-    expect(screen.getByText("this is test text1")).toBeInTheDocument();
-    expect(screen.getByText("this is test text2")).toBeInTheDocument();
+  test("landing on a bad page shows 404 page", () => {
+    const history = createMemoryHistory();
+    history.push("/some/bad/route");
+    const { getByRole } = render(
+      <Router history={history}>
+        <TestComponent />
+      </Router>
+    );
+    expect(getByRole("heading")).toHaveTextContent("404 Not Found");
   });
 });
